e5b1cefc8922e6217cf7d05c0bcaf36089e4f620
hbim <dlog6649@gmail.com>
2021-02-13 17:10:22 +0900

    Modified Button component

--- src/common/components/button/Button.tsx
+++ src/common/components/button/Button.tsx
@@ -1,18 +1,19 @@
-import React, { ReactChildren, useEffect } from "react"
+import React from "react"
 import styles from "./Button.module.scss"
 import cn from "classnames"
 
 type Props = {
-  id?: string | number
   className?: string
-  onClick?: any
+  onClick?: (event: React.MouseEvent<HTMLButtonElement, MouseEvent>) => void
+  icon?: React.ReactNode
+  type?: "ghost"
   children?: any
-  type?: string
 }
 
 export default function Button(p: Props) {
   return (
     <button className={cn(styles.button, p.className)} type={"button"} data-type={p.type} onClick={p.onClick}>
+      {p.icon}
       {p.children}
     </button>
   )
