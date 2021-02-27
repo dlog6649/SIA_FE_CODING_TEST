bc93aae900f8e7273dcc66d181d7e547784d41d8
hbim <dlog6649@gmail.com>
2021-02-27 09:15:53 +0900

    Refactoring

--- src/labeling/page/labeling-view/labeling-board/zoom-slider/ZoomSlider.tsx
+++ src/labeling/page/labeling-view/labeling-board/zoom-slider/ZoomSlider.tsx
@@ -1,29 +1,29 @@
-import React, { useState } from "react"
+import React from "react"
 import { Minus, Plus } from "../../../../../common/asset/icons"
 import cn from "classnames"
-import styles from "./Scaler.module.scss"
+import styles from "./ZoomSlider.module.scss"
 
 type Props = {
   className?: string
+  value: number
+  onChange?: (evt: React.ChangeEvent<HTMLInputElement>) => void
 }
 
-export default function Scaler(p: Props) {
-  const [scale, setScale] = useState<number>(1)
-
+export default function ZoomSlider(p: Props) {
   return (
-    <div className={cn(styles.scaler, p.className)}>
+    <div className={cn(styles.zoomSlider, p.className)}>
       <Plus className={styles.plus} />
       <input
         className={styles.abc}
         type={"range"}
         data-testid={"testScaler"}
-        value={scale}
-        onChange={(evt) => setScale(parseFloat(evt.target.value))}
+        value={p.value}
+        onChange={p.onChange}
         min={"0"}
         max={"2"}
         step={"0.1"}
         style={{
-          background: `linear-gradient(to right, #131644 0%, #131644 ${scale * 50}%,
+          background: `linear-gradient(to right, #131644 0%, #131644 ${p.value * 50}%,
           #d5d4d3 0%, #d5d4d3 100%)`,
         }}
       />
