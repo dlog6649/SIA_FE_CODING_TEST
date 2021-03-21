be48df34e990d568b4ccf5189d2426ad30a52b81
hbim <dlog6649@gmail.com>
2021-03-21 20:08:43 +0900

    Refactoring

--- src/labeling/page/labeling-view/labeling-board/context-menu/ContextMenu.tsx
+++ src/labeling/page/labeling-view/labeling-board/context-menu/ContextMenu.tsx
@@ -46,27 +46,5 @@ export default function ContextMenu(p: Props) {
         <span className={styles.itemShortcut}>Del</span>
       </div>
     </div>
-    // <div className={styles.labelContextmenu + " label-contextmenu"}>
-    //   <div id={"edit"} className={styles.item + " edit"}>
-    //     <span className={styles.itemName}>Edit Class</span>
-    //     <span className={styles.itemShortcut}>(TBD)</span>
-    //   </div>
-    //   <div id={"cut"} className={styles.item + " cut"}>
-    //     <span className={styles.itemName}>Cut</span>
-    //     <span className={styles.itemShortcut}>Ctrl + X</span>
-    //   </div>
-    //   <div id={"copy"} className={styles.item + " copy"}>
-    //     <span className={styles.itemName}>Copy</span>
-    //     <span className={styles.itemShortcut}>Ctrl + C</span>
-    //   </div>
-    //   <div id={"paste"} className={styles.item + " paste"}>
-    //     <span className={styles.itemName}>Paste</span>
-    //     <span className={styles.itemShortcut}>Ctrl + V</span>
-    //   </div>
-    //   <div id={"delete"} className={styles.item + " delete"}>
-    //     <span className={styles.itemName}>Delete</span>
-    //     <span className={styles.itemShortcut}>Del</span>
-    //   </div>
-    // </div>
   )
 }
