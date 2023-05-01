bf753f2d24e5949c86924f5fad85b50e9286ff54
Hyeongbin Im <hbim@si-analytics.ai>
2023-05-01 21:34:46 +0900

    🔧 remove svgrPlugin

--- vite.config.ts
+++ vite.config.ts
@@ -2,14 +2,13 @@ import path from "path"
 
 import react from "@vitejs/plugin-react"
 import { defineConfig } from "vite"
-import svgrPlugin from "vite-plugin-svgr"
 
 // https://vitejs.dev/config/
 export default defineConfig({
   server: {
     port: 5173,
   },
-  plugins: [react(), svgrPlugin()],
+  plugins: [react()],
   resolve: {
     alias: [{ find: "@src", replacement: path.resolve(__dirname, "src") }],
   },
