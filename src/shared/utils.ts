3a5fc221f3a21b78d6406f2c852beb7183ad70b5
Hyeongbin Im <hbim@si-analytics.ai>
2023-05-12 18:49:16 +0900

    ✨ add cn function

--- src/shared/utils.ts
+++ src/shared/utils.ts
@@ -9,3 +9,10 @@ export const pauseEvent = (evt: any) => {
   evt.returnValue = false
   return false
 }
+
+export const cn = (...classNames: (string | number | boolean | undefined | null)[]): string => {
+  return classNames
+    .filter((it) => typeof it === "string" || typeof it === "number")
+    .join(" ")
+    .trim()
+}
